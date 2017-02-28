c7b798bc122e507e4dc0b8d54195142fbb33f0d3
ykalemi <ykalemi@gmail.com>
2017-02-28 12:20:02 +0500

    fixup imports

--- src/main/java/ru/imkn/exceptions/UserNamesApp.java
+++ src/main/java/ru/imkn/exceptions/UserNamesApp.java
@@ -3,7 +3,6 @@ package ru.imkn.exceptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
